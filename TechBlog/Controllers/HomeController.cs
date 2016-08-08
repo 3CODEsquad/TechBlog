1519bc1f211e3b7df883cc6e16669c7604ed2e0e
freakaliciouss <katrin.l.grigorova@gmail.com>
2016-08-08 23:48:12 +0300

    Improvements in About page

--- TechBlog/Controllers/HomeController.cs
+++ TechBlog/Controllers/HomeController.cs
@@ -22,7 +22,17 @@ namespace TechBlog.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "\"The optimist proclaims that we live in the best of all possible worlds,\" observed satirist " +
+                "James Branch Cabell. \"The pessimist fears this is true.\". The oldest debate about technology is the most basic: " +
+                "Is it a force for good or bad? There are many arguments about that, but lets ignore optimists and pessimists, we " +
+                "need to be realists and make this clear that the answer is . . . both. " + 
+                "In these days technology and human life cannot be separated; society has a cyclical co-dependence on technology. " +
+                "We use technology; depend on technology in our daily life and our needs and demands for technology keep on rising. " +
+                "Humans use technology to travel, to communicate, to learn, to do business and to live in comfort. But how all of " +
+                "this affects our lives? " + 
+                "Our blog gives information about various science and technology topics. We concentrate " +
+                "on providing news from different sources and do not limit ourselves on some topics, but try to provide information " +
+                "ranging from pure technical disciplines to natural and social sciences.";
 
             return View();
         }
