adafde690109eee390fa821ee70f14ec32fafd0f
freakaliciouss <katrin.l.grigorova@gmail.com>
2016-08-31 22:18:48 +0300

    New Start page is created; final CSS changes(I hope so!)

--- TechBlog/App_Start/RouteConfig.cs
+++ TechBlog/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TechBlog
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "TechBlog", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
